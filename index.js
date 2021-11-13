433bf6f26efb24df7c4fb2b9c35eb0160636a061
mukitur <mukit.inway@gmail.com>
2021-11-13 13:49:23 +0600

    update

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.adlv3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
 //Main Function start
 
 async function run() {
